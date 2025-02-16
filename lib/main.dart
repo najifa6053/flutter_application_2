7e2a55d2ac395930d2f9561ca045d6f221ccf643
najifa6053 <esha15-6053@diu.edu.bd>
2025-02-16 09:35:21 +0600

    initialize

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyNEW());
+}
+
+class MyNEW extends StatelessWidget {
+  const MyNEW({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
